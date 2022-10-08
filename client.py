c80fb5876a5c02234cf29d9ff0e48d5e7f1ecb8d
Oleksandra Melnyk <oleksandramelnyk.ibm@gmail.com>
2022-10-08 15:30:31 +0300

    f commit

--- client.py
+++ client.py
@@ -2,19 +2,24 @@ import socket
 
 
 def client():
-    host = socket.gethostname()
-    port = 8080
+    try:
+        host = socket.gethostname()
+        port = 8080
 
-    client_socket = socket.socket()
-    client_socket.connect((host, port))
-    message = input('--> ')
+        client_socket = socket.socket()
+        client_socket.connect((host, port))
+        while True:
+            message = input('--> ')
+            if message.lower().strip() == 'bye':
+                print('bye!')
+                break
+            client_socket.send(message.encode())
+            data = client_socket.recv(1024).decode()
+            print(f'{data}')
 
-    client_socket.send(message.encode())
-    data = client_socket.recv(1024).decode()
-    print(f'{data}')
-
-    client_socket.close()
-
+            client_socket.close()
+    except ConnectionRefusedError:
+        print('Server is not running')
 
 if __name__ == '__main__':
     client()
